d3dc95e5ad44539738f5dad4abe87f09708b2f66
Huang096 <huang.zhheng@gmail.com>
2025-01-06 21:46:07 -0800

    home page img ready

--- onlineStore/pages/index/index.vue
+++ onlineStore/pages/index/index.vue
@@ -1,11 +1,22 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<swiper class="swiper-container" indicator-dots="true" autoplay="true" interval="3000" duration="500">
+			<swiper-item>
+				<image src="/static/image1.png" class="slide-image"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="/static/image2.png" class="slide-image"></image>
+			</swiper-item>
+			<swiper-item>
+				<image src="/static/image3.png" class="slide-image"></image>
+			</swiper-item>
+		</swiper>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
 	</view>
 </template>
+
 
 <script>
 	export default {
@@ -31,13 +42,15 @@
 		justify-content: center;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.swiper-container {
+		width: 750rpx; /* 取决于你的设计需求 */
+		height: 400rpx; /* 轮播图的高度，根据需要调整 */
+		margin-top: 50rpx; /* 顶部边距 */
+	}
+
+	.slide-image {
+		width: 100%; /* 让图片充满swiper-item */
+		height: 100%; /* 根据swiper-container的高度自动调整 */
 	}
 
 	.text-area {
@@ -50,3 +63,4 @@
 		color: #8f8f94;
 	}
 </style>
+
